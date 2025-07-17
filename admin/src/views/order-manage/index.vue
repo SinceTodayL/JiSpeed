bc9ebeb7721b5771440fb7fde68f9cc93d005074
SinceToday <3028790558@qq.com>
2025-07-17 17:44:28 +0800

    Modify file structure, Separate user/ and admin/

---
+++ admin/src/views/order-manage/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import HeaderBanner from './modules/header-banner.vue';
+</script>
+
+<template>
+  <NSpace>
+<NCard>
+
+  <HeaderBanner />
+  </NCard>
+    </NSpace>
+
+</template>
